90ab4202ca078886ccc32eca01814b9651eeb1e5
Enzo1994 <120831767@qq.com>
2019-11-02 19:53:20 +0800

    add cropper plugin

--- miniprogram/pages/input-diary/input-diary.js
+++ miniprogram/pages/input-diary/input-diary.js
@@ -7,85 +7,110 @@ Page({
   data: {
     diaryData: {
       media: '',
-      imageUrls: [{
-          url: 'cloud://youxin-d841c0.796f-youxin-d841c0-1251546534/4656e81f6dc57c5.jpg'
-        },
-        {
-          url: 'cloud://youxin-d841c0.796f-youxin-d841c0-1251546534/4656e81f6dc57c5.jpg'
-        }
+      imageUrls: [
+        // {
+        //   url: 'cloud://youxin-d841c0.796f-youxin-d841c0-1251546534/4656e81f6dc57c5.jpg'
+        // },
+        // {
+        //   url: 'cloud://youxin-d841c0.796f-youxin-d841c0-1251546534/4656e81f6dc57c5.jpg'
+        // }
       ]
     }
   },
 
-  addImage: function() {
+  addImage: function () {
     const that = this;
     wx.chooseImage({
-      success: function(res) {
-        console.log(res.tempFilePaths)
+      count: 9 - this.data.diaryData.imageUrls.length,
+      success: function (res) {
         const imageUrls = that.data.diaryData.imageUrls
-        imageUrls.push({
-          url: res.tempFilePaths[0]
+        res.tempFilePaths.forEach(url => {
+          wx.compressImage({
+            src: url,
+            quality: 50,
+            success(tempFilePath) {
+              console.log(tempFilePath.tempFilePath)
+
+              imageUrls.push({
+                url: tempFilePath.tempFilePath
+              })
+
+
+              that.setData({
+                'diaryData.imageUrls': imageUrls
+              })
+
+            }
+          })
         })
-        that.setData({
-          'diaryData.imageUrls': imageUrls
-        })
+
+
       },
     })
+  },
+
+  previewImage: function (e) {
+    console.log(e.currentTarget.dataset.item.url)
+    wx.previewImage({
+      urls: this.data.diaryData.imageUrls.map(i => i.url),// 需要预览的图片http链接列表
+      current: e.currentTarget.dataset.item.url, // 当前显示图片的http链接
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
